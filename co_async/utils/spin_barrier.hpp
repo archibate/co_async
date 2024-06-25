877e7412c3f4e3ddf96328bad381694007095d16
archibate <1931127624@qq.com>
2024-06-25 14:58:58 +0800

    refactor headers

---
+++ co_async/utils/spin_barrier.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <co_async/std.hpp>
+
+namespace co_async {
+
+struct SpinBarrier {
+    explicit SpinBarrier(std::size_t n) noexcept
+        : m_top_waiting((std::uint32_t)n - 1),
+          m_num_waiting(0),
+          m_sync_flip(0) {}
+
+    bool arrive_and_wait() noexcept {
+        bool old_flip = m_sync_flip.load(std::memory_order_relaxed);
+        if (m_num_waiting.fetch_add(1, std::memory_order_relaxed) == m_top_waiting) {
+            m_num_waiting.store(0, std::memory_order_relaxed);
+            m_sync_flip.store(!old_flip, std::memory_order_release);
+            return true;
+        } else {
+            while (m_sync_flip.load(std::memory_order_acquire) == old_flip)
+                ;
+            m_sync_flip.wait(old_flip, std::memory_order_acquire);
+            return false;
+        }
+    }
+
+    bool arrive_and_drop() noexcept {
+        bool old_flip = m_sync_flip.load(std::memory_order_relaxed);
+        if (m_num_waiting.fetch_add(1, std::memory_order_relaxed) == m_top_waiting) {
+            m_num_waiting.store(0, std::memory_order_relaxed);
+            m_sync_flip.store(!old_flip, std::memory_order_release);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+private:
+    std::uint32_t const m_top_waiting;
+    std::atomic<std::uint32_t> m_num_waiting;
+    std::atomic<bool> m_sync_flip;
+};
+
+}

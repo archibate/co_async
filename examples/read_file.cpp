b13748e58438e2b26163bfc05a2d674541aaa788
archibate <1931127624@qq.com>
2024-07-12 14:48:53 +0800

    commreadfileexample

---
+++ examples/read_file.cpp
@@ -0,0 +1,18 @@
+#include <co_async/co_async.hpp>
+#include <co_async/std.hpp>
+
+using namespace co_async;
+using namespace std::literals;
+
+static Task<Expected<>> amain() {
+    auto file = co_await co_await file_open("../examples/read_file.cpp", OpenMode::Read);
+    String buffer = co_await file.getall();
+    co_await co_await stdio().putline(buffer);
+    co_return {};
+}
+
+int main() {
+    IOContext().join(amain()).value();
+    return 0;
+}
+
